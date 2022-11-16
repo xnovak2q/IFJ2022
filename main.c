dfb89b697e30a195b6fd2d88d6d24b60f833afe1
Marek Konečný <marek@konecny.localdomain>
2022-11-16 20:23:47 +0100

    Revert "Token DLList a zaklad pro parser"
    This reverts commit 8445c492f91bc29a706b6dadea8e311589983269.

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 // Created by xfuisl00 on 22.10.2022.
 //
 
-#include "parser.c"
+#include "scanner.c"
 #include "dynamic_string.c"
 #include "basicScannerFunctions.c"
 
 int main(){
-    return runAnalysis();
+    int i = 1;
+    token *token;
+    while(1){
+        token = GetToken();
+        printf("Token #%i Type = %s Value = %s\n", i++, tokenTypos[token->tokenType],token->value->string);
+        if(token->tokenType == end)
+            break;
+        free_token(token);
+    }
 }
