6ca07c2b1b185c75ebfeb4319c713332098c0e98
MattN <matec.novak@seznam.cz>
2022-11-30 17:08:07 +0100

    stack done

--- stack.c
+++ stack.c
@@ -1,23 +1,50 @@
 #include "stack.h"
 
-void stack_initialize(Stack_t* stack){
+void stack_initialize(Stack_t *stack)
+{
     stack->top = NULL;
 };
 
-bool stack_push(Stack_t* stack, item_type type, token* token){
-    Stack_item_t* tmp = (Stack_item_t*) malloc(sizeof(Stack_item_t));
+bool stack_push(Stack_t *stack, item_type type, token *token, bool stop)
+{
+    Stack_item_t *tmp = (Stack_item_t *)malloc(sizeof(Stack_item_t));
 
-    if (!tmp) return false;
-    
+    if (!tmp)
+        return false;
+
     tmp->next = stack->top;
+    stack->top = tmp;
+
     tmp->stack_type = type;
     tmp->token = token;
+    tmp->stop = stop;
+
     return true;
 };
 
-bool stack_pop(Stack_t* stack){
-    if (!(stack->top)) return false;
-    Stack_item_t* tmp = stack->top;
+bool stack_pop(Stack_t *stack)
+{
+    if (!(stack->top))
+        return false;
+
+    Stack_item_t *tmp = stack->top;
     stack->top = tmp->next;
+    free(tmp);
+
     return true;
 };
+
+bool stack_free(Stack_t *stack)
+{
+    if (!(stack))
+        return false;
+
+    while (stack->top)
+    {
+        Stack_item_t *tmp = stack->top;
+        stack->top = tmp->next;
+        free(tmp);
+    }
+
+    return true;
+}
--- stack.h
+++ stack.h
@@ -7,18 +7,18 @@
 typedef enum
 {
     EOS,
-    START,
+    STOP,
     TERM,
     NONTERM
 }item_type;
-
 
 //stack for precedence analysis
 typedef struct stack_item
 {
     token* token;
     item_type stack_type;
-    struct stackitem *next;
+    struct stack_item *next;
+    bool stop;
 }Stack_item_t;
 
 typedef struct stack
@@ -28,8 +28,10 @@ typedef struct stack
 
 void stack_initialize(Stack_t* stack);
 
-bool stack_push(Stack_t* stack, item_type type, token* token);
+bool stack_push(Stack_t* stack, item_type type, token* token, bool stop);
 
 bool stack_pop(Stack_t* stack);
+
+bool stack_free(Stack_t* stack);
 
 #endif
