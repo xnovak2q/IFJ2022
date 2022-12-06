b2716b28b4c01c9d6379f13094f07bf20cd217ae
MattN <matec.novak@seznam.cz>
2022-12-06 23:02:25 +0100

    Infix nearly done (must fix working with list)

--- infix2postfix.c
+++ infix2postfix.c
@@ -1,31 +1,138 @@
 #include "infix2postfix.h"
 
-void untilLeftPar( postStack_t *stack, char *postfixExpression, unsigned *postfixExpressionLength ) {
-    char tmp;
-    while (!Stack_IsEmpty(stack)) //iterujeme dokud není stack prázdný
+bool check_operand(token *token)
+{
+    switch (token->tokenType)
     {
-        Stack_Top(stack, &tmp); //vytáhnutí proměnné ze zásobníku
-        Stack_Pop(stack);
-        if (tmp == '(')
-        {
-            break; //ukončení funkce jakmile se narazí na "(" 
-        }
-        postfixExpression[(*postfixExpressionLength)++] = tmp; //přidání znaku do řetězce a poté zvýšení jeho délky
+    case ffloat:
+    case sstring:
+    case nnull:
+    case integer:
+    case variable:
+        return true;
+    default:
+        return false;
     }
 }
 
-void doOperation( postStack_t *stack, char c, char *postfixExpression, unsigned *postfixExpressionLength ) {
-    if (Stack_IsEmpty(stack)){ //pokud je stack prázdný, přidáme do něj znak a dál se nic neděje (není kolize precedence)
-        Stack_Push(stack, c);
+bool check_operator(token *token)
+{
+    switch (token->tokenType)
+    {
+    case add:
+    case sub:
+    case mul:
+    case ddiv:
+    case concat:
+    case cmpEqual:
+    case notEquals:
+    case lowerEqual:
+    case greaterEqual:
+    case greater:
+    case lower:
+        return true;
+    default:
+        return false;
+    }
+}
+
+postTypes get_operator(token *token)
+{
+    switch (token->tokenType)
+    {
+    case add:
+    case sub:
+    case concat:
+        return plus_minus_dot_p;
+    case mul:
+    case ddiv:
+        return mul_div_p;
+    case cmpEqual:
+    case notEquals:
+        return eq_neq_p;
+    case greater:
+    case lower:
+    case greaterEqual:
+    case lowerEqual:
+        return rel_ops_p;
+    case openBracket:
+        return bracket;
+    }
+}
+
+void untilLeftPar(postStack_t *stack, DLTokenL *postfixExpression)
+{
+    token *tmp;
+    while (!Stack_IsEmpty(stack))
+    {
+        tmp = Stack_Top(stack);
+        Stack_Pop(stack);
+        if (tmp->tokenType == openBracket)
+        {
+            break;
+        }
+        DLTokenL_InsertLast(postfixExpression, tmp);
+    }
+}
+
+void doOperation(postStack_t *stack, token *tok, DLTokenL *postfixExpression)
+{
+    if (Stack_IsEmpty(stack))
+    {
+        Stack_Push(stack, tok);
         return;
     }
-    char tmp;
-    Stack_Top(stack, &tmp); //uložení proměnné z vrcholu stacku do pomocné proměnné
-    if ((tmp == '(') || ((tmp == '+' || tmp == '-') && (c == '*' || c == '/'))){
-        Stack_Push(stack, c);
+    
+    token *tmp = Stack_Top(stack);
+    postTypes stack_type = get_operator(tmp);
+    postTypes input_type = get_operator(tok);
+    if ((stack_type == bracket) ||
+        ((stack_type == plus_minus_dot_p) && (input_type == mul_div_p)) ||
+        ((stack_type == rel_ops_p) && (input_type == mul_div_p || input_type == plus_minus_dot_p)) ||
+        ((stack_type == eq_neq_p) && (input_type != eq_neq_p)))
+    {
+        Stack_Push(stack, tok);
         return;
     }
-    postfixExpression[(*postfixExpressionLength)++] = tmp;
+    DLTokenL_InsertLast(postfixExpression, tmp);
     Stack_Pop(stack);
-    doOperation(stack, c, postfixExpression, postfixExpressionLength);
+    doOperation(stack, tok, postfixExpression);
+}
+
+DLTokenL *infix2postfix(DLTokenL *infixExpression)
+{
+    DLTokenL *new_list = DLTokenL_Create();
+    if (!new_list)
+        exit(99);
+
+    postStack_t *stack = (postStack_t *)malloc(sizeof(postStack_t));
+    if (!stack)
+    {
+        DLTokenL_Dispose(new_list);
+        exit(99);
+    }
+    Stack_Init(stack);
+    DLTokenL_First(infixExpression);
+    token *tok;
+    while (DLTokenL_IsActive(new_list))
+    {
+        tok = DLTokenL_GetActive(infixExpression);
+        if (tok->tokenType == openBracket)
+        {
+            Stack_Push(stack, tok);
+        }
+        else if (tok->tokenType == closeBracket)
+        {
+            untilLeftPar(stack, new_list);
+        }
+        else if (check_operand(tok))
+        {
+            DLTokenL_InsertLast(new_list, tok);
+        }
+        else if (check_operator(tok))
+        {
+            doOperation(stack, tok, new_list);
+        }
+    }
+    free(stack);
 }
--- infix2postfix.h
+++ infix2postfix.h
@@ -13,7 +13,20 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include "postfixStack.h"
+#include "DLTokenList.h"
 
+typedef enum {
+    plus_minus_dot_p,
+    mul_div_p,
+    rel_ops_p,
+    eq_neq_p,
+    bracket
+} postTypes;
 
+bool check_operand(token* token);
+bool check_operator(token* token);
+void untilLeftPar(postStack_t *stack, DLTokenL *postfixExpression);
+void doOperation(postStack_t *stack, token* c, DLTokenL *postfixExpression);
+DLTokenL* infix2postfix( DLTokenL *infixExpression );
 
 #endif
